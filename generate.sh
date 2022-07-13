3e29b13e914a8ca91cddfb2c6c5a5427650c4caf
Situate-hunter <deadirish@mail.ru>
2022-07-13 22:31:08 +0300

    first commit

--- generate.sh
+++ generate.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-random=$(pwgen -A 8 1)
-echo $random
-a=".key"
-b=".crt"
-c=".pfx"
-key="$random$a"
-crt="$random$b"
-pfx="$random$c"
+
 openssl req -new -newkey rsa:2048 -nodes -keyout client.key -x509 -days 365 \ -subj "/C=RU/ST=Tyumen region/L=Tyumen/O=EXAMPLE/OU=CRM/CN=crm.example.ru/emailAddress=crm@example.ru" \ -out client.crt
-openssl pkcs12 -export -out  client.pfx -inkey client.key -in client.crt -passout pass:123ewqasdcxz
+
 openssl req -new -newkey rsa:2048 -nodes -keyout server.key -x509 -days 365 \ -subj "/C=RU/ST=Tyumen region/L=Tyumen/O=EXAMPLE/OU=CRM/CN=crm.example.ru/emailAddress=crm@example.ru" \ -out server.crt
+
